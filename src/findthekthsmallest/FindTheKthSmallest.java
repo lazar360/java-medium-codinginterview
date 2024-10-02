412c1ea4e7361d59820f3e0950ccccf22e51ea8c
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-02 08:47:59 +0200

    update code

--- src/findthekthsmallest/FindTheKthSmallest.java
+++ src/findthekthsmallest/FindTheKthSmallest.java
@@ -2,7 +2,7 @@ package findthekthsmallest;
 
 import java.util.Arrays;
 
-public class FindTheKthSmallestLoop {
+public class FindTheKthSmallest {
 
     public static void main(String[] args) {
 
@@ -10,11 +10,6 @@ public class FindTheKthSmallestLoop {
         int k = 3; // Find the 3rd smallest element
         array = Arrays.stream(array).sorted().toArray();
         int smallest = array[k-1];
-        for (int i = k-1; i < array.length; i++) {
-            if (array[i] < smallest){
-                smallest = array[i];
-            }
-        }
 
         System.out.println(k+"th smallest number : " + smallest);
     }
